7697165a7b0a5a6dddc557c145145751de6138b1
MAFTOUH <omar.maftou0000@gmail.com>
2023-12-10 14:32:16 +0100

    crud user

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/user.model');
+const Subscription = require('../models/subscription.model');
 
 const UserController = {
     countUser: async (req, res) => {
@@ -13,10 +14,49 @@ const UserController = {
     getAllUsers: async (req, res) => {
         try {
             const users = await User.find({}, '-password');
-            res.status(200).json({ users });
+            const usersWithSubscriptionCount = await Promise.all(
+                users.map(async (user) => {
+                    const subscriptionCount = await Subscription.countDocuments({ user: user._id });
+                    return { ...user._doc, subscriptionCount };
+                })
+            );
+
+            res.status(200).json({ users: usersWithSubscriptionCount });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ error: 'Erreur lors de la récupération des utilisateurs' });
+            res.status(500).json({ error: 'Error retrieving users and subscription counts' });
+        }
+    },
+    deleteUser: async (req, res) => {
+        const userId = req.params.id;
+
+        try {
+            const deletedUser = await User.findByIdAndDelete(userId);
+
+            if (deletedUser) {
+                res.status(200).json({ message: 'Utilisateur supprimé avec succès' });
+            } else {
+                res.status(404).json({ error: 'Utilisateur non trouvé' });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erreur lors de la suppression de l\'utilisateur' });
+        }
+    },
+    getUserById: async (req, res) => {
+        const userId = req.params.id;
+
+        try {
+            const user = await User.findById(userId, '-password');
+
+            if (user) {
+                res.status(200).json({ user });
+            } else {
+                res.status(404).json({ error: 'Utilisateur non trouvé' });
+            }
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ error: 'Erreur lors de la récupération de l\'utilisateur' });
         }
     },
 };
--- routes/user.route.js
+++ routes/user.route.js
@@ -5,5 +5,7 @@ const admin = require('../middlewares/admin.middlewares');
 
 router.get('/count-user', admin , UserController.countUser);
 router.get('/all-user', admin , UserController.getAllUsers);
+router.get('/:id', admin , UserController.getUserById);
+router.delete('/:id', admin , UserController.deleteUser);
 
 module.exports = router;
