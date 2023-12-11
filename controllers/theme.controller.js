d0f972504501bdb8471da5c0411308ed7649855e
MAFTOUH <omar.maftou0000@gmail.com>
2023-12-11 20:52:18 +0100

    translate theme  controller

--- controllers/theme.controller.js
+++ controllers/theme.controller.js
@@ -1,4 +1,5 @@
 const Theme = require('../models/theme.model');
+const i18n = require('../config/i18n'); 
 
 const themeController = {
     updateTheme: async (req, res) => {
@@ -9,10 +10,10 @@ const themeController = {
             const theme = await Theme.findOne();
 
             // // Si le thème n'existe pas, créez-le
-            // if (!theme) {
-            //     const newTheme = await Theme.create({ Mybg, MybgHover });
-            //     return res.status(201).json({ status: 'success', data: { theme: newTheme } });
-            // }
+            if (!theme) {
+                const newTheme = await Theme.create({ Mybg, MybgHover });
+                return res.status(201).json({ status: 'success', data: { theme: newTheme } });
+            }
 
             theme.Mybg = Mybg || theme.Mybg;
             theme.MybgHover = MybgHover || theme.MybgHover;
@@ -22,7 +23,7 @@ const themeController = {
             res.status(200).json({ status: 'success', data: { theme } });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ status: 'fail', message: 'Internal server error' });
+            res.status(500).json({ status: 'fail', message: i18n.__('theme.updateTheme.error') });
         }
     },    
     getTheme: async (req, res) => {
@@ -30,12 +31,12 @@ const themeController = {
             const theme = await Theme.findOne();
 
             if (!theme) {
-                return res.status(404).json({ status: 'fail', message: 'Theme not found' });
+                return res.status(404).json({ status: 'fail', message: i18n.__('theme.getTheme.notFound') });
             }
             res.status(200).json({ status: 'success', data: { Mybg: theme.Mybg, MybgHover: theme.MybgHover } });
         } catch (error) {
             console.error(error);
-            res.status(500).json({ status: 'fail', message: 'Internal server error' });
+            res.status(500).json({ status: 'fail', message: i18n.__('theme.getTheme.error') });
         }
     },
 };
