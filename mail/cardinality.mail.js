35a595e8e9f0e9e3848ce836bd5dc75313f661d0
MAFTOUH <omar.maftou0000@gmail.com>
2023-12-17 12:16:08 +0100

    change template email connfirmation

--- mail/cardinality.mail.js
+++ mail/cardinality.mail.js
@@ -14,22 +14,67 @@ const sendCredentialsEmail = ( user ) => {
     const mailOptions = {
         from: process.env.EMAIL,
         to: user.email,
-        subject: "Your Login Credentials",
+        subject: "Steam Sign Up Verification (code: Clean Cow)",
         html: `
-            <div style="background-color: #ecfccb; padding: 50px 30px; display: flex; justify-content: center; align-items: center; height: 100vh;">
-                <div style="background-color: #f7fee7; padding: 30px; border-radius: 10px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);text-align: center;">
-                    <h2 style="color: #1e293b; margin-bottom: 20px; font-size: 24px;">Greetings, ${user.FirstName}&nbsp;${user.LastName}!</h2>
-                    <p style="font-size: 18px; margin-bottom: 20px; color: #374151;">Your login credentials are ready:</p>
-                    <ul style="font-size: 18px; list-style-type: none; padding-left: 0; margin-bottom: 20px;">
-                        <li><strong style="color: #1e293b;">First Name:</strong> ${user.FirstName}</li>
-                        <li><strong style="color: #1e293b;">Last Name:</strong> ${user.LastName}</li>
-                        <li><strong style="color: #1e293b;">Email:</strong> ${user.email}</li>
-                        <li><strong style="color: #1e293b;">Password:</strong> ${user.password}</li>
-                    </ul>
-                    <a href='${confirmationLink}' style="display: inline-block; padding: 12px 60px; background-color: #84cc16; color: #fff; text-decoration: none; border-radius: 5px; transition: background-color 0.3s; font-size: 18px;">Verify Account</a>
-                    <p style="font-size: 18px; color: #374151; margin-top: 20px;">Thank you for choosing our service at Steam.</p>
-                </div>
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            <div class="adM">
+            </div><table width="100%" border="0" cellspacing="0" cellpadding="0" style="width:100%!important">
+              <tbody><tr><td align="center">
+            <table style="border:1px solid #eaeaea;border-radius:5px;margin:40px 0" width="600" border="0" cellspacing="0" cellpadding="40">
+              <tbody><tr><td align="center"><div style="font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;text-align:left;width:465px">
+            
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" style="width:100%!important">
+              <tbody><tr><td align="center">
+                <h1>Welcome ${user.FirstName}</h1>
+              <h1 style="color:#333;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:18px;font-weight:normal;margin:30px 0;padding:0">Verify your email to sign up for <b>Steam</b></h1>
+            </td></tr>
+            </tbody></table>
+            
+            <p style="color:#000;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:14px;line-height:24px">
+                An attempt to sign up has been made with the provided code
+            </p>
+            <br>
+            
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" style="width:100%!important">
+              <tbody><tr>
+                <td align="center" bgcolor="#f6f6f6" valign="middle" height="40" style="font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:16px;font-weight:bold">Clean Cow</td>
+              </tr>
+            </tbody></table>
+            
+            <br>
+            <p style="color:#000;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:14px;line-height:24px">To complete the signup process, please click on the button below. Please note that by completing your signup you are agreeing to our <a href="#" style="color:#067df7;text-decoration:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://vercel.com/terms&amp;source=gmail&amp;ust=1702896804218000&amp;usg=AOvVaw1_l_JNABJ-CAow-VUnHIFT">Terms of Service</a> and <a href="#" style="color:#067df7;text-decoration:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://vercel.com/privacy&amp;source=gmail&amp;ust=1702896804218000&amp;usg=AOvVaw2dlcx4jMTLCeECQwslANYP">Privacy Policy</a>:</p>
+            <br>
+            
+            <table width="100%" border="0" cellspacing="0" cellpadding="0" style="width:100%!important">
+              <tbody><tr><td align="center">
+            <div>
+              
+                <a href='${confirmationLink}' style="background-color:#000;border-radius:5px;color:#fff;display:inline-block;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:12px;font-weight:500;line-height:50px;text-align:center;text-decoration:none;width:200px" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://vercel.com/confirm?email%3Domar.ort0000%2540gmail.com%26token%3DGdXqyMfvGnniTBs3fmN5o2Ud%26mode%3Dsignup&amp;source=gmail&amp;ust=1702896804218000&amp;usg=AOvVaw1ASWN_5C0rkZmWLluDYbIe">VERIFY</a>
+              
             </div>
+            </td></tr>
+            </tbody></table>
+            
+            <br>
+            <p style="color:#000;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:14px;line-height:24px">Or copy and paste this URL into a new tab of your browser:</p>
+            <p style="color:#000;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:14px;line-height:24px"><a href="${confirmationLink}" style="color:#067df7;text-decoration:none" target="_blank" data-saferedirecturl="https://www.google.com/url?q=https://vercel.com/confirm?email%3Domar.ort0000%2540gmail.com%26token%3DGdXqyMfvGnniTBs3fmN5o2Ud%26mode%3Dsignup&amp;source=gmail&amp;ust=1702896804218000&amp;usg=AOvVaw1ASWN_5C0rkZmWLluDYbIe">${confirmationLink}</a></p>
+            <br>
+            <hr style="border:none;border-top:1px solid #eaeaea;margin:26px 0;width:100%">
+            <p style="color:#666666;font-family:-apple-system,BlinkMacSystemFont,&quot;Segoe UI&quot;,&quot;Roboto&quot;,&quot;Oxygen&quot;,&quot;Ubuntu&quot;,&quot;Cantarell&quot;,&quot;Fira Sans&quot;,&quot;Droid Sans&quot;,&quot;Helvetica Neue&quot;,sans-serif;font-size:12px;line-height:24px">If you didn't attempt to sign up but received this email, </p>
+            </div></td></tr>
+            </tbody></table>
+            </td></tr>
+            </tbody></table><div class="yj6qo"></div><div class="adL">
+            </div>
+        </body>
+        </html>
         `};
 
     transporter.sendMail(mailOptions, (error, info) => {
