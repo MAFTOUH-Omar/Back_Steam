1b5f749eaedbc6040e57d85a8d7b906874d1030b
Yassine Nassibi <ynassibi@e3r2p1.1337.ma>
2024-05-01 15:26:51 +0100

    stripe done !

--- app.js
+++ app.js
@@ -83,8 +83,8 @@ app.post("/stripe", async (req, res) => {
           quantity: item.quantity,
         };
       }),
-      success_url: `${process.env.CLIENT_URL}/success.html`,
-      cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
+      // success_url: `${process.env.CLIENT_URL}/success.html`,
+      // cancel_url: `${process.env.CLIENT_URL}/cancel.html`,
     });
     res.json({ url: session.url });
   } catch (e) {
