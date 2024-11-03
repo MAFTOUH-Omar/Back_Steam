5525b40625596766fcbb8eb70c5602cf13913229
MAFTOUH <omar.maftou0000@gmail.com>
2024-11-04 00:43:22 +0100

    adding middelware auth in subscription routes

--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -5,13 +5,13 @@ const auth = require('../middlewares/auth.middlewares');
 const admin = require('../middlewares/admin.middlewares');
 
 router.post('/create-subscription' , SubscriptionController.createSubscription)
-router.put('/extend-subscription' , SubscriptionController.extendSubscription)
+router.put('/extend-subscription' , auth ,SubscriptionController.extendSubscription)
 router.get('/count-subscription' , admin , SubscriptionController.countSubscriptions);
 router.get('/getAllSubscriptionsWithUserAndPackage' , admin , SubscriptionController.getAllSubscriptionsWithUserAndPackage);
-router.get('/subscription-user' , SubscriptionController.getAllSubscriptionsByUserId);
-router.get('/:subscriptionId' , SubscriptionController.getSubscriptionById);
+router.get('/subscription-user' , auth , SubscriptionController.getAllSubscriptionsByUserId);
+router.get('/:subscriptionId' , auth , SubscriptionController.getSubscriptionById);
 router.get('/subscription-admin/:subscriptionId' , admin , SubscriptionController.getSubscriptionById);
-router.put('/updateSubscription' , SubscriptionController.updateSubscription);
+router.put('/updateSubscription' , auth , SubscriptionController.updateSubscription);
 router.put('/updateSubscription-admin' , admin , SubscriptionController.updateSubscription);
 router.post('/disabelSubscription' , admin , SubscriptionController.disableSubscription);
 router.post('/enableSubscription' , admin , SubscriptionController.enableSubscription);
